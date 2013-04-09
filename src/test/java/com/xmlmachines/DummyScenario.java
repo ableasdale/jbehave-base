37104f021335a4cd5d9940d6cc9b9d869e717065
Alex Bleasdale <ableasdale@marklogic.com>
2013-04-09 13:32:18 +0100

    maven

---
+++ src/test/java/com/xmlmachines/DummyScenario.java
@@ -0,0 +1,40 @@
+package com.xmlmachines;
+
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 09/04/13
+ * Time: 13:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class DummyScenario extends JUnitStory {
+
+    // Here we specify the configuration, starting from default MostUsefulConfiguration, and changing only what is needed
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration()
+                // where to find the stories
+                .useStoryLoader(new LoadFromClasspath(this.getClass()))
+                        // CONSOLE and TXT reporting
+                .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(StoryReporterBuilder.Format.CONSOLE, StoryReporterBuilder.Format.TXT, StoryReporterBuilder.Format.HTML, StoryReporterBuilder.Format.XML));
+    }
+
+    // Here we specify the steps classes
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new SampleSteps());
+    }
+
+
+}
--- src/test/resources/Simple.story
+++ src/test/resources/Simple.story
@@ -5,17 +5,16 @@ Meta:
 @color blue
 
 Narrative:
-
-We want to do some sums.
+    As a mathematician
+    I want to do some sums
+    In order to make JBehave work
 
 Scenario: 2 squared
-
 Given a variable x with value 2
 When I multiply x by 2
 Then x should equal 4
 
 Scenario: 3 squared
-
 Given a variable x with value 3
 When I multiply x by 3
-Then x should equal 10
+Then x should equal 9
