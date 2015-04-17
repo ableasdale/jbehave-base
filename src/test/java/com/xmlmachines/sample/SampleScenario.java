6970a37a5a264237cb9015e6487eff514458a727
Alex Bleasdale <alex.bleasdale@marklogic.com>
2015-04-17 09:10:56 -0700

    Minor update to base
    Updated Maven dependencies and removed older deprecated code

--- src/test/java/com/xmlmachines/sample/SampleScenario.java
+++ src/test/java/com/xmlmachines/sample/SampleScenario.java
@@ -1,10 +1,12 @@
 package com.xmlmachines.sample;
 
 import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
@@ -19,7 +21,9 @@ public class SampleScenario extends JUnitStory {
                 // Loading stories from classpath
                 .useStoryLoader(new LoadFromClasspath(this.getClass()))
                         // CONSOLE and TXT reporting
-                .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(StoryReporterBuilder.Format.CONSOLE, StoryReporterBuilder.Format.TXT, StoryReporterBuilder.Format.HTML, StoryReporterBuilder.Format.XML));
+                
+                .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(Format.CONSOLE, Format.TXT)); 
+               // .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(StoryReporterBuilder.Format.CONSOLE, StoryReporterBuilder.Format.TXT, StoryReporterBuilder.Format.HTML, StoryReporterBuilder.Format.XML));
     }
 
     // Inject Steps
